e50c763f87839ecfe62c5920ea8c27f84b742d86
Daniel Sanchez <daniel.ds.1302@gmail.com>
2022-07-13 18:07:40 -0500

    Subida

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', {
+      useNewUrlParser: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
     }),
